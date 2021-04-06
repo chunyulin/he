fd8d8abd4ae57da85eb0e3dd7d398511da24e056
Chun-Yu Lin <lincy@nchc.org.tw>
2021-04-06 17:46:01 +0800

    Simple codes for comparing different libraries.

--- compare/pali_sq_sum.cpp
+++ compare/pali_sq_sum.cpp
@@ -12,11 +12,10 @@ std::chrono::time_point<std::chrono::system_clock>  tc, tw, t, t1;
 #define TIC(t) t = std::chrono::system_clock::now();
 #define TOC(t) (std::chrono::system_clock::now() - t)
 
-
 std::chrono::time_point<std::chrono::system_clock> t_tmp, t_global;
 
 DURATION t_pk, t_sk, t_rk, t_gk;        /// key related.
-DURATION t_input, t_enc, t_dec, t_square, t_batch_sum, t_reduct_sum;   /// computation related.
+DURATION t_input, t_enc, t_dec, t_batch_sum, t_reduct_sum;   /// computation related.
 DURATION t_raw, t_total;
 
 using namespace std;
@@ -30,25 +29,13 @@ int main(int argc, char* argv[]) {
 
     TIC(t_global);
     
-    //**************************************************
-    // Instantiate the crypto context and key generation
-    //**************************************************
     SecurityLevel securityLevel = HEStd_128_classic;
     //SecurityLevel securityLevel = HEStd_NotSet;
 
-#define FASTROT
-#ifdef FASTROT
     usint nMults   = 1;        // max depth of tower
-#else
-    usint nMults   = 2;        // max depth of tower
-#endif
-    usint maxdepth = 1;        // max key for s^i 
-    /// maxdepth : determines the capability of relinearization key.
-
-    usint numLargeDigits = 0;
+    usint maxdepth = 1;        // max key for s^i : determines the capability of relinearization key.
+    usint numLargeDigits = nMults + 1;
     int relinwin = 0;
-
-    cout << "Initializing crypto context..." << endl;
 
     auto cc = CryptoContextFactory<DCRTPoly>::genCryptoContextCKKS(
             nMults, scaleFactor, batchSize, securityLevel, ringDimension,
@@ -62,25 +49,12 @@ int main(int argc, char* argv[]) {
 
     // Output the generated parameters
     auto ccParam = cc->GetCryptoParameters();
+    cout << "# of Cores        : " << omp_get_max_threads() << endl;
     cout << "   Ring dimension : " << cc->GetRingDimension() << endl;
-    cout << "   p     : " << ccParam->GetPlaintextModulus() << endl;
-    cout << "   log2q : " << log2(cc->GetModulus().ConvertToDouble())  << endl;
-    
-    cout << "=================================================" << endl;
-    cout << "Plaintext  Modulus : " << ccParam -> GetPlaintextModulus() << endl;
-//     cout << "Ciphertext Modulus : " << ccParam -> GetCiphertextModulus() << endl; /// doesn't work? 
-    cout << "Cyclotomic Order   : " << ccParam -> GetElementParams() -> GetCyclotomicOrder() << endl;
     cout << "   log2q : " << log2( cc-> GetModulus().ConvertToDouble()) << endl;
-    cout << "   ScaleFactor    : " << scaleFactor << endl;
-    cout << "   ScaleFactor[T] : " << cc-> GetEncodingParams() << endl;
-    cout << "=================================================" << endl;
+    cout << "   ScaleFactor[T] : " << cc->GetEncodingParams() << endl;
     
     
-    /// Load the binary file as input.
-    //TIC(t_tmp);
-    //vector<double> x = read_bin(input_bin);
-    //unsigned long N = x.size();
-    //t_input = TOC(t_tmp);
     int num_batch = 10;
     int N = batchSize*num_batch;
     
@@ -102,9 +76,7 @@ int main(int argc, char* argv[]) {
 
     TIC(t1);
     cout << "Generating sum key ..." << endl;
-#ifndef FASTROT
     cc->EvalSumKeyGen(keyPair.secretKey);
-#endif
     DURATION tSumKG = TOC(t1);
 
     TIC(t1);
@@ -112,42 +84,19 @@ int main(int argc, char* argv[]) {
     cc->EvalMultKeyGen(keyPair.secretKey);
     DURATION tMultKG = TOC(t1);
 
-    //********************************************************
-    //
-    // Full rotation key for merging partial sum in each batch
-    //
-    //********************************************************
-    TIC(t1);
-    cout << "Generating full rotation keys ..." << endl << endl;
-#ifdef FASTROT
-    int nr = log2(batchSize);
-    vector<int> ilist(nr);
-    for (int i=0;i<nr;i++)   ilist[i] = 1<<i;
-    cc->EvalAtIndexKeyGen(keyPair.secretKey, ilist);
-#else
-    vector<int> ilist(num_batch-1);
-    #pragma omp parallel for
-    for (int i=0;i<num_batch-1;i++) ilist[i]=-(i+1);
-    cc->EvalAtIndexKeyGen(keyPair.secretKey, ilist);
-#endif
-    DURATION tRotKG = TOC(t1);
 
     //**************************************************
     //
-    // Caculate \sum sigmoid(x) w/o HE
+    // Caculate \sum x^2 w/o HE
     //
     //**************************************************
     cout << "Native computation w/o HE ..." << endl;
-    double exact_sum = 0.;
+    double raw_result = 0.;
     
     TIC(t);
-    #pragma omp parallel for reduction(+ : exact_sum)
+    #pragma omp parallel for reduction(+ : raw_result)
     for (usint i=0; i<N; i++) {
-#ifdef SQUARE
-        exact_sum += x[i]*x[i];
-#else
-        exact_sum += x[i];
-#endif
+        raw_result += x[i]*x[i];
     }
     DURATION t_raw = TOC(t);
 
@@ -180,53 +129,28 @@ int main(int argc, char* argv[]) {
     vector<Ciphertext<DCRTPoly>> bctsum(num_batch);
     //int num_rotation = log2(batchSize);
     //int M = 2*cc->GetRingDimension();
-    
+    DURATION t_square;
     #pragma omp parallel for
     for (int i = 0; i < num_batch; i++) {
-#ifdef SQUARE
-#if defined(FASTROT)
-        //bctsum[i] = cc->EvalMult(ctx[i], ctx[i]);
-        bctsum[i] = cc->EvalMultAndRelinearize(ctx[i], ctx[i]);
-        Ciphertext<DCRTPoly> crot;
-	for (int j=1;j<=nr;j++) {
-    	    crot = cc->EvalAtIndex(bctsum[i], (1<<(nr-j)));
-	    cc->EvalAddInPlace(bctsum[i], crot);
-	}                
-#else
-        auto x2   = cc->EvalMult(ctx[i], ctx[i]);
-        bctsum[i] = cc->EvalSum(x2, batchSize);
-#endif
 
-#else
+    TIC(t1);
+        auto ctx2 = cc->EvalMult(ctx[i], ctx[i]);
+    t_square += TOC(t1);
+    bctsum[i] = cc->EvalSum(ctx2, batchSize);
 
-#if defined(FASTROT)
-        bctsum[i] = ctx[i];
-	//auto cPrecomp = cc->EvalFastRotationPrecompute(bctsum[i]);
-        Ciphertext<DCRTPoly> crot;
-	for (int j=1;j<=nr;j++) {
-    	    crot = cc->EvalAtIndex(bctsum[i], (1<<(nr-j)));
-    	    //crot = cc->EvalFastRotation(bctsum[i], 1<<(nr-j), M, cPrecomp);
-	    cc->EvalAddInPlace(bctsum[i], crot);
-	}
-#else
-        bctsum[i] = cc->EvalSum(ctx[i], batchSize);
-#endif
-#endif
     }
     cout <<endl << endl;
     DURATION tEvalAll = TOC(t);
+
+    // TODO: OMP optimization
     TIC(t);
     cout << "Merging partial sum." << endl << endl;
-#ifdef FASTROT
     auto ctsum = bctsum[0];
     for (int i=1;i<num_batch; i++) {
         cc->EvalAddInPlace(ctsum, bctsum[i]);
     }
-#else
-    auto ctsum = cc -> EvalSum(cc->EvalMerge(bctsum), num_batch);
-#endif    
-    
     DURATION tMerge = TOC(t);
+
 
     TIC(t);
     Plaintext res;
@@ -241,46 +165,19 @@ int main(int argc, char* argv[]) {
         Summarize the results.
     *************************************/
     
-    /// align palisade & seal.
     t_pk  = tKG;
-    t_gk  = tRotKG;
+    t_gk  = tSumKG;
     t_rk  = tMultKG;
     t_enc = tEncAll;
     t_dec = tDec;
-    t_square = tEvalAll;
     t_batch_sum  = tEvalAll;
     t_reduct_sum = tMerge;
-    double raw_result = exact_sum;
     
     double error = he_result - raw_result;
         
-                printf( "CKKS Result : %.6f\n", he_result);
-                    printf( "Raw  Result : %.6f\n", raw_result);
+    printf( "CKKS Result : %.6f\n", he_result);
+    printf( "Raw  Result : %.6f\n", raw_result);
         
-    vector<double> out_vec(20);
-    
-    /// statistics for key generation.
-    out_vec[0] = t_pk.count();
-    out_vec[1] = t_sk.count();
-    out_vec[2] = t_gk.count();
-    out_vec[3] = t_rk.count();  
-    \\
-    /// statistics for computations.
-    out_vec[4] = t_input.count();
-    out_vec[5] = t_enc.count();
-    out_vec[6] = t_dec.count();
-    out_vec[7] = t_square.count(); //t_square.count(); 
-    out_vec[8] = t_batch_sum.count(); //t_batch_sum.count(); 
-    out_vec[9] = t_reduct_sum.count(); //t_reduct_sum.count();
-    out_vec[10] = t_raw.count();
-    out_vec[11] = t_total.count();
-    out_vec[12] = raw_result;
-    out_vec[13] = he_result;
-    out_vec[14] = num_batch;
-    out_vec[15] = tSumKG.count();
-    
-    
-    cout << "======== Performance    ========" << endl;
     cout << "======== Key Generation ========" << endl;
     cout << "Public Key        : " << t_pk.count() <<endl;
     cout << "Secret Key Gen    : " << t_sk.count() <<endl;
@@ -291,12 +188,10 @@ int main(int argc, char* argv[]) {
     cout << "Prepare Input     : " << t_input.count() <<endl;
     cout << "Encode/Encrypt    : " << t_enc.count() <<endl;
     cout << "Decode/Decrypt    : " << t_dec.count() <<endl;
-    cout << "Enc. Squaring     : " << t_square.count() <<endl;
-    cout << "Enc. Sum [batch]  : " << t_batch_sum.count() / num_batch <<endl;
+    cout << "Squaring per b    : " << t_square.count() / num_batch <<endl;
+    cout << "Compute  per b    : " << t_batch_sum.count() / num_batch <<endl;
     cout << "Enc. Sum [reduce] : " << t_reduct_sum.count() <<endl;
     cout << "Error             : " << setprecision(16) << error << endl;
-    cout << "# of Cores        : " << omp_get_max_threads() << endl;
-    cout << tSumKG.count();
     
     
     return 0;
--- compare/seal_sq_sum.cpp
+++ compare/seal_sq_sum.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <omp.h>
+#include <iomanip>
 
 /// For performance profiling.
 #include <chrono>
@@ -13,7 +14,7 @@ typedef  std::chrono::duration<double> DURATION;
 #define TIC(t) t = std::chrono::system_clock::now();
 #define TOC(t) (std::chrono::system_clock::now() - t);
 
-std::chrono::time_point<std::chrono::system_clock> t_tmp, t_global;
+std::chrono::time_point<std::chrono::system_clock> t_tmp, t_global, t;
 
 DURATION t_pk, t_sk, t_rk, t_gk;        /// key related.
 DURATION t_input, t_enc, t_dec, t_square, t_batch_sum, t_reduct_sum;   /// computation related.
@@ -27,38 +28,29 @@ int main(int argc, char* argv[])
     // Get the num of cores.
     int num_cores   = omp_get_max_threads();
     int num_threads = omp_get_num_threads();
-    string input_bin = "1M.bin";
-    string dst_base  = "";
-    
+
     cout << "==== # of cores   : " << num_cores   << " ==== " << endl;
     cout << "==== # of threads : " << num_threads << " ==== " << endl;
-    
+
     if (argc >= 3) {
         num_threads = atoi(argv[1]);
-        input_bin   = argv[2];
-        dst_base    = argv[3];
     }
     TIC(t_global);
-        
-    /// Set openMP threads.
+
     //omp_set_num_threads(num_threads);
     omp_set_num_threads(num_cores);
-    
-    // Encryption parameters. 
+
+    // Encryption parameters.
     EncryptionParameters parms(scheme_type::ckks);
 
     // Set prms for polynomial ring.
     int batchSize=8192;
-    //int batchSize=4096;
     size_t poly_modulus_degree = batchSize*2;
     parms.set_poly_modulus_degree(poly_modulus_degree);
     parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, { 60, 40, 40, 60 }));
     //parms.set_coeff_modulus( { 0x7fffffd8001, 0x7fffffc8001, 0xfffffffc001, 0xffffff6c001, 0xfffffebc001 } );
-    //parms.set_coeff_modulus( { 0xfffffffd8001, 0xfffffffa0001, 0xfffffff00001, 0x1fffffff68001, 0x1fffffff50001, 
-    //                                                                    0x1ffffffee8001, 0x1ffffffea0001, 0x1ffffffe88001, 0x1ffffffe48001 } );
+    //auto p128 = seal::util::global_variables::GetDefaultCoeffModulus128();
 
-   //auto p128 = seal::util::global_variables::GetDefaultCoeffModulus128();
-   
     double scale = pow(2.0, 39);
 
     // Print the context.
@@ -86,7 +78,7 @@ int main(int argc, char* argv[])
     TIC(t_tmp);
     keygen.create_relin_keys(relin_keys);
     t_rk = TOC(t_tmp);
-    
+
     GaloisKeys gal_keys;
     TIC(t_tmp);
     keygen.create_galois_keys(gal_keys);
@@ -107,27 +99,13 @@ int main(int argc, char* argv[])
     cout << "==== Number of slots: " << slot_count << endl;
     cout << "==== End of key generation & context creation ====" << endl;
     cout << "==================================================" << endl;
-   
-    /// Load the binary file as input.
-    //TIC(t_tmp);
-    //vector<double> input = read_bin(input_bin);
-    //unsigned long N = input.size();
-    //t_input = TOC(t_tmp);
 
     int num_batch = 10;
     int N = batchSize*num_batch;
     std::vector<double> x(N);
-    #pragma omp parallel for
+#pragma omp parallel for
     for (int i=0; i<N; i++) x[i] = (double) (rand()-RAND_MAX/2) / (RAND_MAX);
 
-    
-    //cout << "Input vector: " << endl;
-    //print_vector(input, 3, 7);
-
-    //int num_batch = N/slot_count + int(N%slot_count!=0);
-    
-    // print_line(__LINE__);
-    
     // Encode & Encrypt.
     vector<Ciphertext> cipher_txts(num_batch);
     unsigned int batch_sz = slot_count;
@@ -135,39 +113,35 @@ int main(int argc, char* argv[])
     cout << "# of data  : " << N << endl; 
     cout << "num_batch  : " << num_batch << endl;
     cout << "slot_count : " << slot_count << endl;
-    
+
     TIC(t_tmp);
     double raw_result = 0.0;
-    
+
     /// calculate the raw result.
-    #pragma omp parallel for reduction(+ : raw_result)
+#pragma omp parallel for reduction(+ : raw_result)
     for(int i=0; i < x.size(); i++){
-#ifdef SQUARE
         raw_result += x[i]*x[i];
-#else
-        raw_result += x[i];
-#endif
     }
     t_raw = TOC(t_tmp);
-    
+
     /// Encode & Encrypt.
     Plaintext  x_plain;
     Ciphertext x_encrypted;
-    
+
     TIC(t_tmp);
-    #pragma omp parallel for 
+#pragma omp parallel for 
     for (int i=0; i< num_batch-1; i++){
 
         Plaintext  x_plain;
         Ciphertext x_encrypted;
-        
+
         vector<double> x_batch = {x.begin() + i*batch_sz, x.begin() + (i+1)*batch_sz};
         /// encode & encrypt.
         encoder.encode(x_batch, scale, x_plain); 
         encryptor.encrypt(x_plain, x_encrypted);
         cipher_txts[i] = x_encrypted;
     }
-    
+
     /// deal with the last batch.
     vector<double> x_batch = {x.begin() + (num_batch-1)*batch_sz, x.end()};
     encoder.encode(x_batch, scale, x_plain);    
@@ -175,81 +149,79 @@ int main(int argc, char* argv[])
     cipher_txts[num_batch-1] = x_encrypted;
 
     t_enc = TOC(t_tmp);
-//     cout << "Encode/Encrypt : " << t_enc.count() << endl;
+    //     cout << "Encode/Encrypt : " << t_enc.count() << endl;
     cout << "End of encode/encrypt..." << endl;
 
     /// do the HE computations.
     /// calculate the square.
     cout << "Compute x^2 and relinearize..." << endl;
-    
-    
+
+
     /// sum the whole slot with rotations.
     int num_rotation = log2(slot_count);
     vector<double> result_vec(slot_count);
     double he_result = 0.0;
-    
+    t_square = {};
+
     TIC(t_tmp);
-    #pragma omp parallel for
+#pragma omp parallel for
     for (int i=0; i < num_batch; i++){
-        
-#ifdef SQUARE
+
+        TIC(t);
         evaluator.square_inplace(cipher_txts[i]);
         evaluator.relinearize_inplace(cipher_txts[i], relin_keys);
         evaluator.rescale_to_next_inplace(cipher_txts[i]);
-#endif
-//         Ciphertext rotated_sum = cipher_txts[i];
+        t_square += TOC(t);
+
         Ciphertext rotated;
-        
         /// sum the batch by rotation.
         for (int j=1; j <= num_rotation; j++){
             int rot_steps = (int)pow(2.0, num_rotation-j);
             evaluator.rotate_vector(cipher_txts[i], rot_steps, gal_keys, rotated);
             evaluator.add(cipher_txts[i], rotated, cipher_txts[i]);
         } 
-//         cipher_txts[i] = rotated_sum;
     }
     t_batch_sum = TOC(t_tmp);
-    
+
+
     TIC(t_tmp);
-   
-    
     Plaintext plain_result;
     Ciphertext cipher_result = cipher_txts[0];
-    
+
     TIC(t_tmp);
     for (int i=1; i < num_batch; i++){
         evaluator.add(cipher_result, cipher_txts[i], cipher_result);
     }
     t_reduct_sum = TOC(t_tmp);
-    
-    
+
+
     TIC(t_tmp);
     decryptor.decrypt(cipher_result, plain_result); 
     encoder.decode(plain_result, result_vec);
     he_result = result_vec[0];
     t_dec = TOC(t_tmp);
-    
+
     /// time for whole calculation.
     t_total = TOC(t_global);
-    
+
     cout << "Decrypted   : " << t_dec.count() << endl;
     printf( "CKKS Result : %.6f\n", he_result);
     printf( "Raw  Result : %.6f\n", raw_result);
-    
-    
+
+
     /************************************
-        Summarize the results.
-    *************************************/
+      Summarize the results.
+     *************************************/
     double error = he_result - raw_result;
-    
+
     vector<double> out_vec(20);
-    
+
     /// statistics for key generation.
     out_vec[0] = t_pk.count();
     out_vec[1] = t_sk.count();
     out_vec[2] = t_gk.count();
     out_vec[3] = t_rk.count(); 
-    
+
     /// statistics for computations.
     out_vec[4] = t_input.count();
     out_vec[5] = t_enc.count();
@@ -263,23 +235,23 @@ int main(int argc, char* argv[])
     out_vec[13] = he_result;
     out_vec[14] = num_batch;
     out_vec[15] = 0; // correspond to SumKey in palisade.
-    
-    cout << "======== Performance    ========" << endl;
-    cout << "======== Key Generation ========" << endl;
+
+    cout << "======== Key Generation   ========" << endl;
     cout << "Public Key        : " << t_pk.count() <<endl;
     cout << "Secret Key Gen    : " << t_sk.count() <<endl;
     cout << "Rotation Key      : " << t_gk.count() <<endl;
     cout << "Relinearilzation  : " << t_rk.count() <<endl;
-    
+
     cout << "======== Computations   ========" << endl;
     cout << "Prepare Input     : " << t_input.count() <<endl;
     cout << "Encode/Encrypt    : " << t_enc.count() <<endl;
     cout << "Decode/Decrypt    : " << t_dec.count() <<endl;
-    cout << "Enc. Squaring     : " << t_square.count() <<endl;
-    cout << "Enc. Sum [batch]  : " << t_batch_sum.count() / num_batch <<endl;
-    cout << "Enc. Sum [reduce] : " << t_reduct_sum.count() <<endl;
+    cout << "Squaring per b    : " << t_square.count() / num_batch <<endl;
+    cout << "Compute  per b    : " << t_batch_sum.count() / num_batch <<endl;
+    cout << "Enc. Sum [reduce] : " << t_reduct_sum.count() <<endl<<endl;
+    cout << "Error             : " << std::setprecision(16) << error << endl;
     
-    
+
     return 0;
 }
 
